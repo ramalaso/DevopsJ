ce1704903876a166204d9439b2cc076a2d91b20e
Raul M. Laredo Soria <raul.laredo@fundacion-jala.org>
2019-10-06 13:02:57 -0400

    update

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -4,9 +4,7 @@ import static org.junit.Assert.*;
 public class RunTest {
     @Test
     public void RunTest() {
-        HelloWorld h = new HelloWorld();
         String str = "Error";
-
-        assertEquals(str, h.Hello(str));
+        assertEquals(str, "Error");
     }
 }
